35ebc04b0e70411368eaea5cc1603b7f2b83fdba
Nicolay J <njoya@softserveinc.com>
2024-02-22 14:25:24 -0500

    optimizing fonts loading

--- src/content/ResumeCard/MainIntro.jsx
+++ src/content/ResumeCard/MainIntro.jsx
@@ -6,7 +6,7 @@ export function MainIntroduction () {
       <section className='introduction-section'>
         <h2 className='introduction-text'><span className='import-text'>Middle Engineer</span> at <span className='library-import-text'> '@Softserve'</span></h2>
         <p className='introduction-text'>
-          Blending bytes and dreams since 2018 ✌️.
+          Blending bytes and dreams since 2018 <span className='victory-animation'>✌🏻</span>
         </p>
       </section>
     </>
